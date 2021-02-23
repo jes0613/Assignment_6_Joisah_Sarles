20d96420ac5599d163d174f6b356670b7487e99b
jes0613 <46938121+jes0613@users.noreply.github.com>
2021-02-23 16:26:03 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Assignment_6_Joisah_Sarles.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment_6_Joisah_Sarles.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        //Added a private repository
+        private IFamazonRepo _repo;
+
+        // recieves the logger and the repository then sets the private values
+        public HomeController(ILogger<HomeController> logger, IFamazonRepo repo)
+        {
+            _logger = logger;
+            _repo = repo;
+        }
+
+        // sends the _repo.books to the view to be used to print on the index page
+        public IActionResult Index()
+        {
+            return View(_repo.books);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment_6_Joisah_Sarles.Models
+{
+    //The book class, has the fields needed for the projects as listed in the spec
+    //All the fields are required and the bookId is set as the Key
+    public class Book
+    {
+        [Key]
+        public int bookId { get; set; }
+        [Required]
+        public string title { get; set; }
+
+        [Required]
+        public string author { get; set; }
+
+        [Required]
+        public string publisher { get; set; }
+
+        [Required]
+        // Checks the isbn to make sure it's in the right format
+        [RegularExpression(@"^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$",
+            ErrorMessage =" ISBN in not in the required format of ###-##########")]
+        public string isbn { get; set; }
+
+        [Required]
+        public string category { get; set; }
+
+        [Required]
+        public double price { get; set; }
+
+
+
+
+    }
+}
